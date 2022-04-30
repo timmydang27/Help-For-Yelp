2db09b20da122e3a9703b01dbb51112ea9411111
aachin22 <aachin@mit.edu>
2022-04-30 14:03:42 -0400

    cleaned business.csv

---
+++ business_clean.py
@@ -0,0 +1,11 @@
+#from distutils.command.build_ext import build_ext
+import pandas as pd
+
+business = pd.read_json('./yelp_academic_dataset_business.json', lines=True)
+business = business[['business_id', 'name', 'city', 'state', 'stars', 'review_count', 'categories', 'is_open']]
+
+business = business[(business['is_open']==1) & (business['state']=='CA')]\
+    .reset_index()
+business = business[['business_id', 'name', 'city', 'state', 'stars', 'review_count', 'categories']]
+
+business.to_csv('business.csv')
